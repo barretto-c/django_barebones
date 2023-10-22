5391368ad92130a4f291b5fac0ef48cbd821a951
ClydeB <barretto.clyde@gmail.com>
2023-10-22 17:02:04 -0400

    Adding tests

--- django_barebones/polls/tests.py
+++ django_barebones/polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+    def test_index_view_returns_200(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_returns_expected_content(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response, '<h1>Simple Django Website</h1>')
+        self.assertContains(response, '<p>Just saying hello</p>')
+        self.assertContains(response, '<a href=" admin/">Admin Site</a>')
